ced2b60690cf23767f221b112867e0ed55e8b722
krishna102001 <krishnakantmaurya@icloud.com>
2024-01-19 13:35:58 +0530

    add a route to send the index.html

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,6 +33,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.listen(4000, () => {
   console.log("server running on localhost:4000");
 });
